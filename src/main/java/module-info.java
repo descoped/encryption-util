0312eb08fd403a30218eb9970b17b7f5980c41b3
oranheim <oranheim@gmail.com>
2020-02-25 10:58:35 +0100

    Removed jackson dependency

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,6 @@
 module no.ssb.rawdata.encryption {
 
     requires java.base;
-    requires com.fasterxml.jackson.databind;
-    requires com.fasterxml.jackson.core;
 
     exports no.ssb.rawdata.payload.encryption;
 }
